f356bcee466a726a9875dcc4fc10715badd25e9a
aidenbradley <mraidenbradley@gmail.com>
2022-09-04 22:25:36 +0100

    Adding InteractsWithEntities trait to introduce a few helper methods when interacting with entities in tests

---
+++ tests/src/Kernel/Support/InteractsWithEntitiesTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\Tests\test_support\Kernel\Support;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
+use Drupal\node\NodeInterface;
+use Drupal\Tests\test_support\Traits\Support\InteractsWithEntities;
+
+class InteractsWithEntitiesTest extends KernelTestBase
+{
+    use InteractsWithEntities;
+
+    protected static $modules = [
+        'system',
+        'node',
+        'user',
+    ];
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->installEntitySchema('node');
+        $this->installEntitySchema('user');
+
+        $this->installSchema('node', 'node_access');
+
+        NodeType::create([
+            'type' => 'page',
+            'name' => 'Basic page',
+        ])->save();
+    }
+
+    /** @test */
+    public function create_entity(): void
+    {
+        $node = $this->createEntity('node', [
+            'title' => 'Example node',
+            'type' => 'page',
+        ]);
+
+        $this->assertInstanceOf(NodeInterface::class, $node);
+        $this->assertEquals('Example node', $node->title->value);
+    }
+
+    /** @test */
+    public function update_entity(): void
+    {
+        $node = $this->createEntity('node', [
+            'title' => 'Example node',
+            'type' => 'page',
+        ]);
+
+        $this->assertInstanceOf(NodeInterface::class, $node);
+        $this->assertEquals('Example node', $node->title->value);
+
+        $this->updateEntity($node, [
+            'title' => 'Updated Example Title',
+        ]);
+
+        $this->assertInstanceOf(NodeInterface::class, $node);
+        $this->assertEquals('Updated Example Title', $node->title->value);
+    }
+}
