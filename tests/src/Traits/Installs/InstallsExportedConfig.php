619031c3777e42893ce2e8dcce45fd3c1493d478
aidenbradley <mraidenbradley@gmail.com>
2022-12-20 23:25:43 +0000

    Remove unused use statements

--- tests/src/Traits/Installs/InstallsExportedConfig.php
+++ tests/src/Traits/Installs/InstallsExportedConfig.php
@@ -2,11 +2,6 @@
 
 namespace Drupal\Tests\test_support\Traits\Installs;
 
-use Drupal\Core\Config\FileStorage;
-use Drupal\Core\Site\Settings;
-use Drupal\Tests\test_support\Traits\Support\Exceptions\ConfigInstallFailed;
-use Drupal\Tests\test_support\Traits\Support\InteractsWithSettings;
-
 trait InstallsExportedConfig
 {
     use InstallsFields,
