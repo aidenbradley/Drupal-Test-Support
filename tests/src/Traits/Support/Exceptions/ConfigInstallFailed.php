edd799406c579b5c5036c97c3650276d4c0548be
aidenbradley <mraidenbradley@gmail.com>
2023-05-11 21:41:34 +0100

    Fix mess detection on ConfigInstallFailed named exception

--- tests/src/Traits/Support/Exceptions/ConfigInstallFailed.php
+++ tests/src/Traits/Support/Exceptions/ConfigInstallFailed.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\test_support\Traits\Support\Exceptions;
 
+use Throwable;
+
 class ConfigInstallFailed extends \Exception
 {
     public const CONFIGURATION_DOES_NOT_EXIST = 1;
@@ -9,37 +11,26 @@ class ConfigInstallFailed extends \Exception
     public const FAILED_HANDLING_CONFIGURATION = 2;
 
     /** @var string */
-    private $failingConfigFile = '';
+    private $failingConfigFile;
+
+    public function __construct(string $failingConfigFile, string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->failingConfigFile = $failingConfigFile;
+    }
 
     public static function doesNotExist(string $configFile): self
     {
-        $exception = new self(
+        return new self(
+            $configFile,
             'Configuration file ' . $configFile . ' does not exist',
             self::CONFIGURATION_DOES_NOT_EXIST
         );
-
-        return $exception->setFailingConfigFile($configFile);
-    }
-
-    public static function couldNotHandle(string $configFile): self
-    {
-        $exception = new self(
-            'The following configuration has failed to import ' . $configFile,
-            self::FAILED_HANDLING_CONFIGURATION
-        );
-
-        return $exception->setFailingConfigFile($configFile);
     }
 
     public function getFailingConfigFile(): string
     {
         return $this->failingConfigFile;
-    }
-
-    private function setFailingConfigFile(string $configFile): self
-    {
-        $this->failingConfigFile = $configFile;
-
-        return $this;
     }
 }
